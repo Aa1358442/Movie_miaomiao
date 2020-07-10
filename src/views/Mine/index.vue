d51ae08d71b0cf42476b13174643073dd675e842
itman <351522421@qq.com>
2020-07-10 10:52:21 +0800

    first commit

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵"/>
+            <div id="content">
+                <Login />
+            </div>
+        <TarBar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header/index.vue'
+import TarBar from '@/components/TarBar/index.vue'
+import Login  from '@/components/Login/index.vue'
+
+export default {
+    name: 'Mine',
+    components: {
+        Header,
+        TarBar,
+        Login
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
